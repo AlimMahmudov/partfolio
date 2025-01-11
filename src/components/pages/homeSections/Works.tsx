7f2c00327e63e7049bd881f9eaa989a8b64f38e3
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-11 21:47:23 +0600

    images

--- src/components/pages/homeSections/Works.tsx
+++ src/components/pages/homeSections/Works.tsx
@@ -3,9 +3,15 @@ import scss from "../homeSections/Works.module.scss";
 import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { BsBoxArrowUpRight } from "react-icons/bs";
-import photo1 from "../../../assets/Снимок экрана от 2024-08-04 03-31-56.png";
-import photo2 from "../../../assets/Снимок экрана от 2024-08-04 03-00-51.png";
-import photo3 from "../../../assets/Снимок экрана от 2024-08-04 03-03-43.png";
+// import photo1 from "../../../assets/Снимок экрана от 2024-08-04 03-31-56.png";
+// import photo2 from "../../../assets/Снимок экрана от 2024-08-04 03-00-51.png";
+// import photo3 from "../../../assets/Снимок экрана от 2024-08-04 03-03-43.png";
+
+import restaran from "@/assets/images/Снимок экрана от 2025-01-11 21-33-55.png";
+import barber from "@/assets/images/Снимок экрана от 2025-01-11 21-35-29.png";
+import course from "@/assets/images/Снимок экрана от 2025-01-11 21-36-35.png";
+import ortodont from "@/assets/images/Снимок экрана от 2025-01-11 21-37-05.png";
+import starwars from "@/assets/images/Снимок экрана от 2025-01-11 21-37-41.png";
 
 const Works = () => {
   return (
@@ -38,15 +44,14 @@ const Works = () => {
                 </div>
               </div>
               <div className={scss.image}>
-                <Image className={scss.photo1} src={photo1} alt="" />
+                <Image className={scss.photo1} src={restaran} alt="" />
               </div>
             </div>
             {/* ////// */}
 
             <div className={scss.card2}>
               <div className={scss.image}>
-                ``
-                <Image className={scss.photo2} src={photo2} alt="" />
+                <Image className={scss.photo2} src={barber} alt="" />
               </div>
               <div className={scss.text}>
                 <h1>Temir</h1>
@@ -73,13 +78,14 @@ const Works = () => {
 
             <div className={scss.card}>
               <div className={scss.text}>
-                <h1>Jordan</h1>
+                <h1>Call of Duty</h1>
                 <p>
-                  The credit card website provides information on various types
-                  of credit cards, their terms, interest rates and bonuses.
-                  Users can compare cards, read reviews, get advice on choosing
-                  and managing loans, and learn about current promotions and
-                  offers.
+                  Call of Duty: Modern Warfare 3 (MW3) is a website dedicated to
+                  the popular video game. The site provides information about
+                  the game, including the plot, characters, gameplay, and modes.
+                  Users can find news and updates, trailers, screenshots, and
+                  other media. The section also contains frequently asked
+                  questions, guides, and a forum for discussing games.
                 </p>
                 <div className={scss.box}>
                   <h3>
@@ -91,11 +97,63 @@ const Works = () => {
                 </div>
               </div>
               <div className={scss.image}>
-                <Image className={scss.photo3} src={photo3} alt="" />
+                <Image className={scss.photo1} src={ortodont} alt="" />
+              </div>
+            </div>
+            {/* ////// */}
+
+            <div className={scss.card2}>
+              <div className={scss.image}>
+                <Image className={scss.photo2} src={starwars} alt="" />
+              </div>
+              <div className={scss.text}>
+                <h1>Temir</h1>
+                <p>
+                  Jordan I&apos;s sneaker site offers information on models and
+                  collections, including detailed descriptions, photos and
+                  reviews. The site provides information on the brand I&apos; s
+                  history, news on upcoming releases and exclusive collections.
+                  Users can make purchases, subscribe to updates and participate
+                  in discussions.
+                </p>
+                <div className={scss.box}>
+                  <h3>
+                    Code <FaGithub />
+                  </h3>
+                  <h3>
+                    Live Demo <BsBoxArrowUpRight />
+                  </h3>
+                </div>
               </div>
             </div>
 
-            {/* ///// */}
+            {/* ////// */}
+
+            <div className={scss.card}>
+              <div className={scss.text}>
+                <h1>Call of Duty</h1>
+                <p>
+                  Call of Duty: Modern Warfare 3 (MW3) is a website dedicated to
+                  the popular video game. The site provides information about
+                  the game, including the plot, characters, gameplay, and modes.
+                  Users can find news and updates, trailers, screenshots, and
+                  other media. The section also contains frequently asked
+                  questions, guides, and a forum for discussing games.
+                </p>
+                <div className={scss.box}>
+                  <h3>
+                    Code <FaGithub />
+                  </h3>
+                  <h3>
+                    Live Demo <BsBoxArrowUpRight />
+                  </h3>
+                </div>
+              </div>
+              <div className={scss.image}>
+                <Image className={scss.photo1} src={course} alt="" />
+              </div>
+            </div>
+            {/* ////// */}
           </div>
         </div>
       </div>
