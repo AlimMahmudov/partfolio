5c584a79a18068c9a6187e28051ab348b1bff6d9
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-14 22:33:47 +0600

    skill steck

--- src/components/pages/homeSections/Skill.tsx
+++ src/components/pages/homeSections/Skill.tsx
@@ -1,93 +1,133 @@
-// import React from "react";
-// import scss from "../homeSections/Skill.module.scss";
-
-// import { FaHtml5 } from "react-icons/fa";
-// import { FaCss3Alt } from "react-icons/fa";
-// import { IoLogoJavascript } from "react-icons/io5";
-// import { SiTypescript } from "react-icons/si";
-// import { SiSass } from "react-icons/si";
-// import { FaReact } from "react-icons/fa";
-// import { SiRedux } from "react-icons/si";
-// import { SiVite } from "react-icons/si";
-// import { RiNextjsFill } from "react-icons/ri";
-// import { FaGitAlt } from "react-icons/fa";
-// import { FaGithub } from "react-icons/fa";
-// import { FaFigma } from "react-icons/fa6";
-// import { SiVisualstudiocode } from "react-icons/si";
-// import { RiTailwindCssFill } from "react-icons/ri";
-
-// const Skill = () => {
-//   return (
-//     <section id="skill" className={scss.Skill}>
-//       <div className="container">
-//         <div className={scss.content}>
-//           <div className={scss.textStack}>
-//             <div className={scss.line}></div>
-//             <h1>MY SKILL</h1>
-//             <div className={scss.line}></div>
-//           </div>
-//           <div className={scss.images}>
-//             <div className={scss.stack1}>
-//               <FaHtml5 />
-//             </div>
-//             <div className={scss.stack2}>
-//               <FaCss3Alt />
-//             </div>
-//             <div className={scss.stack3}>
-//               <IoLogoJavascript />
-//             </div>
-//             <div className={scss.stack4}>
-//               <SiTypescript />
-//             </div>
-//             <div className={scss.stack5}>
-//               <SiSass />
-//             </div>
-//             <div className={scss.stack6}>
-//               <FaReact />
-//             </div>
-//             <div className={scss.stack7}>
-//               <SiRedux />
-//             </div>
-//             <div className={scss.stack8}>
-//               <SiVite />
-//             </div>
-//             <div className={scss.stack9}>
-//               <RiNextjsFill />
-//             </div>
-//             <div className={scss.stack10}>
-//               <SiVisualstudiocode />
-//             </div>
-//             <div className={scss.stack11}>
-//               <FaFigma />
-//             </div>
-//             <div className={scss.stack12}>
-//               <FaGithub />
-//             </div>
-//             <div className={scss.stack13}>
-//               <FaGitAlt />
-//             </div>
-//             <div className={scss.stack14}>
-//               <RiTailwindCssFill />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Skill;
-
 import React from "react";
 import scss from "../homeSections/Skill.module.scss";
 
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
+import { SiSass } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { SiVite } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa6";
+import { SiVisualstudiocode } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+
 const Skill = () => {
   return (
-    <div id="skill" className={scss.Skill}>
+    <section id="skill" className={scss.Skill}>
       <div className="container">
-        <div className={scss.skill}></div>
+        <div className={scss.content}>
+          <div className={scss.textStack}>
+            <div className={scss.line}></div>
+            <h1>MY SKILL</h1>
+            <div className={scss.line}></div>
+          </div>
+          <div className={scss.images}>
+            <div className={scss.stack1}>
+              <h1>
+                <FaHtml5 />
+              </h1>
+              <h2>HTML</h2>
+            </div>
+
+            <div className={scss.stack2}>
+              <h1>
+                <FaCss3Alt />
+              </h1>
+              <h2>CSS</h2>
+            </div>
+
+            <div className={scss.stack3}>
+              <h1>
+                <IoLogoJavascript />
+              </h1>
+              <h2>JS</h2>
+            </div>
+
+            <div className={scss.stack4}>
+              <h1>
+                <SiTypescript />
+              </h1>
+              <h2>TS</h2>
+            </div>
+
+            <div className={scss.stack5}>
+              <h1>
+                <SiSass />
+              </h1>
+              <h2>SCSS</h2>
+            </div>
+
+            <div className={scss.stack6}>
+              <h1>
+                <FaReact />
+              </h1>
+              <h2>React</h2>
+            </div>
+
+            <div className={scss.stack7}>
+              <h1>
+                <SiRedux />
+              </h1>
+              <h2>Redux</h2>
+            </div>
+
+            <div className={scss.stack8}>
+              <h1>
+                <SiVite />
+              </h1>
+              <h2>React-Vite</h2>
+            </div>
+
+            <div className={scss.stack9}>
+              <h1>
+                <RiNextjsFill />
+              </h1>
+              <h2>NextJS</h2>
+            </div>
+
+            <div className={scss.stack10}>
+              <h1>
+                <SiVisualstudiocode />
+              </h1>
+              <h2>CSS</h2>
+            </div>
+
+            <div className={scss.stack11}>
+              <h1>
+                <FaFigma />
+              </h1>
+              <h2>Figma</h2>
+            </div>
+
+            <div className={scss.stack12}>
+              <h1>
+                <FaGithub />
+              </h1>
+              <h2>GitHub</h2>
+            </div>
+
+            <div className={scss.stack13}>
+              <h1>
+                <FaGitAlt />
+              </h1>
+              <h2>Git</h2>
+            </div>
+
+            <div className={scss.stack14}>
+              <h1>
+                <RiTailwindCssFill />
+              </h1>
+              <h2>Tailwind</h2>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
