846b27d84c7781219aabc98c27258c2243293a9e
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-16 19:38:01 +0600

    about

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
     <>
       {/* <Spider id="particles" /> */}
       {/* <Welcome /> */}
-      {/* <AboutMe /> */}
+      <AboutMe />
       <Works />
       {/* <Work /> */}
       {/* <Concat /> */}
