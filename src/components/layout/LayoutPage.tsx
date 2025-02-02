4c3c675b0f0b8475e0fa822193f285b4d2cb8231
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-02-02 23:15:49 +0600

    login

--- src/components/layout/LayoutPage.tsx
+++ src/components/layout/LayoutPage.tsx
@@ -1,41 +1,3 @@
-// "use client";
-// import React, { FC, ReactNode, useEffect, useState } from "react";
-// import scss from "../layout/LayoutPage.module.scss";
-// import Header from "./header/Header";
-// import Footer from "./footer/Footer";
-// import LoginM14 from "@/login/LoginM14";
-
-// interface LayoutPageProps {
-//   children: ReactNode;
-// }
-
-// const LayoutPage: FC<LayoutPageProps> = ({ children }) => {
-//   const [loading, setLoading] = useState<boolean>(true);
-
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setLoading(false);
-//     }, 2000);
-//   }, []);
-//   return (
-//     <div className={scss.LayoutPage}>
-//       {loading ? (
-//         <>
-//           <LoginM14 />
-//         </>
-//       ) : (
-//         <>
-//           <Header />
-//           <main>{children}</main>
-//           <Footer />
-//         </>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default LayoutPage;
-
 "use client";
 import React, { FC, ReactNode, useEffect, useState } from "react";
 import scss from "../layout/LayoutPage.module.scss";
@@ -48,15 +10,53 @@ interface LayoutPageProps {
 }
 
 const LayoutPage: FC<LayoutPageProps> = ({ children }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <div className={scss.LayoutPage}>
-      <>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </>
+      {loading ? (
+        <>
+          <LoginM14 />
+        </>
+      ) : (
+        <>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
 
 export default LayoutPage;
+
+// "use client";
+// import React, { FC, ReactNode, useEffect, useState } from "react";
+// import scss from "../layout/LayoutPage.module.scss";
+// import Header from "./header/Header";
+// import Footer from "./footer/Footer";
+// import LoginM14 from "@/login/LoginM14";
+
+// interface LayoutPageProps {
+//   children: ReactNode;
+// }
+
+// const LayoutPage: FC<LayoutPageProps> = ({ children }) => {
+//   return (
+//     <div className={scss.LayoutPage}>
+//       <>
+//         <Header />
+//         <main>{children}</main>
+//         <Footer />
+//       </>
+//     </div>
+//   );
+// };
+
+// export default LayoutPage;
